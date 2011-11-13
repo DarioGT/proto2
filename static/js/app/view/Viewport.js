2df074f37bc1df9d471704cf9e87c4c4a7746d01
Dario Gomez <dariogomezt@gmail.com>
2011-11-13 17:05:40 -0500

    Layout

--- static/js/app/view/Viewport.js
+++ static/js/app/view/Viewport.js
@@ -1,32 +1,62 @@
 Ext.define('ProtoUL.view.Viewport', {
-    extend: 'Ext.container.Viewport', 
+    extend : 'Ext.container.Viewport',
 
     //requires: ['Ext.toolbar.Paging'],
-    layout: 'border',
-    defaults: {
-        collapsible: true,
-        split: true
+    layout : 'border',
+    defaults : {
+        collapsible : true,
+        split : true
     },
-    items: [{
+    items : [{
+        // xtype: 'forumlist',
+        region : 'west',
+        width : 300,
+        // title : 'Menu',
+        layout: 'accordion',
+        items:[{
+            title: 'Menu',
+            html: 'Content'
+        },{
+            title: 'Favorits',
+            id: 'panel2',
+            html: 'Content'
+        }]
+    }, {
+        // xtype: 'properties',
+        region : 'east',
+        width : 300,
+        title : 'Properties',
+        collapsed : true,
+    }, {
+
+
         // title: 'Master',
         // tbar: tb,
-        region: 'center',
-        layout: 'fit',
-        collapsible: false,
-        // items: masterGrid
-        xtype: 'contactlist'
-    }, {
-        title: 'Details',
-        region: 'south',
-        collapsed: true,
-        layout: 'fit',
-        height: 180,
-        minSize: 75,
-        defaults: {
-            border: false,
-            activeTab: 0
+        region : 'center',
+        collapsible : false,
+        layout : 'border',
+        defaults : {
+            collapsible : true,
+            split : true
         },
+
+        items : [{
+            region : 'center',
+            layout : 'fit',
+
+            collapsible : false,
+            // items: masterGrid
+            xtype : 'contactlist'
+        }, {
+            title : 'Details',
+            region : 'south',
+            collapsed : false,
+            layout : 'fit',
+            height : 180,
+            minSize : 75,
         // items: protoTabs,
+
+        }]
     }]
 
 });
