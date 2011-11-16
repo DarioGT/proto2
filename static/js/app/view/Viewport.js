c84eb614f31017ace8cb58973555283529f9d307
Dario Gomez <dariogomezt@gmail.com>
2011-11-16 13:27:22 -0500

    Separacion de bloques basica

--- static/js/app/view/Viewport.js
+++ static/js/app/view/Viewport.js
@@ -6,79 +6,123 @@ Ext.define('ProtoUL.view.Viewport', {
         'ProtoUL.view.Properties',
     ],
 
-    //requires: ['Ext.toolbar.Paging'],
-    layout: 'border',
-    
-    // Dgt para q se desplegue 
-    autoRender: true, 
-    defaults: {
-        collapsible: true,
-        split: true
+    initComponent: function(){
+
+        Ext.apply(this, {
+            layout: 'border',
+            autoRender: true, 
+            padding: 5,
+            defaults: {
+                collapsible: true,
+                split: true
+            },
+            items: [
+                this.createProtoPanel(),
+                this.createMenuPanel(),
+                // this.createPropertyPanel(),
+                // this.createHeaderPanel(),
+                // this.createFooterPanel(),
+                ],
+                
+        });
+
+        this.callParent(arguments);
+        
     },
-    items: [{
-        xtype: 'box',
-        region:'north',
-        html: '<span class="title">Proto Certae </span><span class="subtitle">Version 0.0</span>',
-        height: 40,
-        collapsible: false,
-        split: false,
-    },{
-        // xtype: 'forumlist',
-        region: 'west',
-        width: 300,
-        // title : 'Menu',
-        layout: 'accordion',
-        items: [{
-            title: 'Menu',
-            layout: 'fit',
-            xtype: 'menuTree'
-            // xtype: 'treepanel',
-            
-        }, {
-            title: 'Favorits',
-            // id: 'panel2',
-            // html: 'Content'
-        }]
-    }, {
-        // xtype: 'properties',
-        region: 'east',
-        width: 300,
-        title: 'Properties',
-        collapsed: true,
-        xtype : 'properties',
-    }, {
-        // title: 'Master',
-        // tbar: tb,
-        region: 'center',
-        collapsible: false,
-        layout: 'border',
-        defaults: {
-            collapsible: true,
-            split: true
-        },
 
-        items: [{
+    createMenuPanel: function(){
+        // this.menuPanel = Ext.create('widget.menupanel', {
+        this.menuPanel = {
+            region: 'west',
+            width: 300,
+            title : 'Menu',
+            layout: 'accordion',
+            items: [{
+                // title: 'Menu',
+                layout: 'fit',
+                xtype: 'menuTree'
+                // xtype: 'treepanel',
+            }, {
+                title: 'Favorits',
+                hidden: true, 
+            }]            
+        }
+        // );
+
+        // listeners: {
+            // scope: this,
+            // feedselect: this.onFeedSelect
+        // };
+
+        return this.menuPanel;
+    },
+
+
+    createProtoPanel: function(){
+        // this.protoPanel = Ext.create('widget.protopanel', {
+        this.protoPanel = {
+            // title: 'Master',
+            // tbar: tb,
             region: 'center',
-            layout: 'fit',
-
             collapsible: false,
-            // items: masterGrid
-            xtype: 'contactlist'
-        }, {
-            title: 'Details',
-            region: 'south',
-            collapsed: false,
-            layout: 'fit',
-            height: 180,
-            minSize: 75,
-            // items: protoTabs,
-        }]
-    }, {
-        region: 'south',
-        collapsible: false,
-        split: false,
-        height: 20,
-    }
-    ]
+            layout: 'border',
+            defaults: {
+                collapsible: true,
+                split: true
+            },
+    
+            items: [{
+                region: 'center',
+                layout: 'fit',
+    
+                collapsible: false,
+                // items: masterGrid
+                xtype: 'contactlist'
+            }, {
+                title: 'Details',
+                region: 'south',
+                collapsed: false,
+                layout: 'fit',
+                height: 180,
+                minSize: 75,
+                // items: protoTabs,
+            }]
+        }; 
+        return this.protoPanel;
+    },
+
+    // createHeaderPanel: function(){
+        // this.headerPanel = Ext.create('widget.headerpanel', {
+            // xtype: 'box',
+            // region:'north',
+            // html: '<span class="title">Proto Certae </span><span class="subtitle">Version 0.0</span>',
+            // height: 40,
+            // collapsible: false,
+            // split: false,
+        // });
+        // return this.headerPanel;
+    // },
+
+    // createPropertyPanel: function(){
+        // this.propertyPanel = Ext.create('widget.propertypanel', {
+            // region: 'east',
+            // width: 300,
+            // title: 'Properties',
+            // collapsed: true,
+            // xtype : 'properties',
+        // });
+        // return this.propertyPanel;
+    // },
+
+
+    // createFooterPanel: function(){
+        // this.footerPanel = Ext.create('widget.footerpanel', {
+            // region: 'south',
+            // collapsible: false,
+            // split: false,
+            // height: 20,
+        // });
+        // return this.footerPanel;
+    // },
 
 });
